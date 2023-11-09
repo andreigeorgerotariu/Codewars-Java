64d8fcf1523b59d4f2221fa5ad10b82e29ffe130
lpandrew <lp_andrew@yahoo.com>
2023-11-09 18:30:33 +0200

    solved 'Even or Odd' Codewars problem

---
+++ src/codewars/eightKyu/EvenOrOdd.java
@@ -0,0 +1,13 @@
+package codewars.eightKyu;
+
+//https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+public class EvenOrOdd {
+
+    public static String evenOrOdd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else
+            return "Odd";
+    }
+}
