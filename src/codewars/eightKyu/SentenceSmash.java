83088ba84cfbbcdb389b7de04739458e101d3c88
lpandrew <lp_andrew@yahoo.com>
2023-11-10 14:53:02 +0200

    solved 'Sentence Smash' Codewars problem

---
+++ src/codewars/eightKyu/SentenceSmash.java
@@ -0,0 +1,9 @@
+package codewars.eightKyu;
+
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
+
+public class SentenceSmash {
+    public static String smash(String... words) {
+        return String.join(" ", words);
+    }
+}
