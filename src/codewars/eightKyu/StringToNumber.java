7d25d94a2afdbc7e556b4a2469d92d5e7c5a8599
lpandrew <lp_andrew@yahoo.com>
2023-11-11 21:16:49 +0200

    solved 'Convert a String to a Number!' Codewars problem

---
+++ src/codewars/eightKyu/StringToNumber.java
@@ -0,0 +1,9 @@
+package codewars.eightKyu;
+
+// https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+public class StringToNumber {
+    public static int stringToNumber(String str) {
+        return Integer.parseInt(str);
+    }
+}
